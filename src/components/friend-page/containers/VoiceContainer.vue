a8dfd5b456ba34a198d23b3dd2997ff1311251c3
Christos Tanailidis <ctanailidis@lancom.gr>
2023-05-11 01:44:25 +0300

    make things work again

--- src/components/friend-page/containers/VoiceContainer.vue
+++ src/components/friend-page/containers/VoiceContainer.vue
@@ -1,19 +1,9 @@
 <template>
-  <div>
-    <button @click="startCall">
-      Start Call
-    </button>
-    <button @click="endCall">
-      End Call
-    </button>
-  </div>
+  <div />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import SimplePeer from 'simple-peer'
-import wrtc from 'wrtc'
-
 // components
 
 // models
@@ -24,42 +14,7 @@ import wrtc from 'wrtc'
 
 export default defineComponent({
   setup () {
-    const peerRef = ref<SimplePeer.Instance | null>(null)
-
-    const startCall = () => {
-      const peer = new SimplePeer({
-        initiator: true,
-        wrtc
-      })
-
-      peer.on('signal', (data) => {
-      // Send the signal data to the other peer
-      })
-
-      peer.on('connect', () => {
-      // Connection established
-      })
-
-      peer.on('data', (data) => {
-      // Receive audio data from other peer
-      })
-
-      peer.on('close', () => {
-      // Connection closed
-      })
-
-      peerRef.value = peer
-    }
-
-    const endCall = () => {
-      if (peerRef.value) {
-        peerRef.value.destroy()
-        peerRef.value = null
-      }
-    }
     return {
-      startCall,
-      endCall
     }
   }
 })

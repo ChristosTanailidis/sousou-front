d40eec3f0bceae6cd5936a85a4516a3794f161f8
Christos Tanailidis <ctanailidis@lancom.gr>
2024-02-15 02:45:51 +0200

    test turn server

--- src/stores/call-store.ts
+++ src/stores/call-store.ts
@@ -6,7 +6,13 @@ import { socket } from 'src/boot/socket_io'
 import { PersonalMessage } from 'src/models/PersonalMessage'
 import { useAuthUser } from './auth-user'
 
-const PEER_CONNECTION_CONFIG = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }
+const PEER_CONNECTION_CONFIG = {
+  iceServers: [{
+    urls: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+  }]
+}
 
 export const useCallStore = defineStore('call-store', {
   state: () => ({
